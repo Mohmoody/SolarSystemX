7b6fcfe524b2dac22792b933447b51a002c17729
Mohmoody <syed.mahmoody2014@gmail.com>
2021-06-15 21:59:31 -0400

    planet2 is ready

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,5 +12,10 @@ public class Main {
         Feron.Heat="6000000000kj";
         Feron.Radius="868465656113613251km/2";
         Feron.Temperature="10000000000000000000000f";
+
+        Planet2 cx2=new Planet2();
+        cx2.heat="67858758765";
+        cx2.radious="473456sdfg5km/5";
+
     }
 }
---
+++ src/com/company/Planet2.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Planet2 {
+    String radious;
+    String heat;
+}
