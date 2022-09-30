d8d02a8d02cd8bfb049931e6cfa57e581346b296
Mohmoody <syed.mahmoody2014@gmail.com>
2022-09-30 02:19:02 -0400

    planet 2 is ready

--- src/main/java/com/solar/Main.java
+++ src/main/java/com/solar/Main.java
@@ -15,5 +15,9 @@ public class Main {
         feron.Radius="23425yu";
         feron.Temperature="67578920000000f00000000000000000000000000000000000000000000000";
 
+        Planet2 esay=new Planet2();
+        esay.size="234234";
+        esay.radius="45346365";
+
     }
 }
---
+++ src/main/java/com/solar/Planet2.java
@@ -0,0 +1,6 @@
+package com.solar;
+
+public class Planet2 {
+    String radius;
+    String size;
+}
