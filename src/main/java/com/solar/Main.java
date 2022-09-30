2cb98cb513b4ef858aa33b7eefc3b9e8c13189e1
Mohmoody <syed.mahmoody2014@gmail.com>
2022-09-29 21:17:57 -0400

    solary system basic structure is ready and deployed

--- src/main/java/com/solar/Main.java
+++ src/main/java/com/solar/Main.java
@@ -2,6 +2,11 @@ package com.solar;
 
 public class Main {
     public static void main(String[] args) {
+//developing the basic solar system
+        SolarSystem factorX=new SolarSystem();
 
+        factorX.sun=1;
+        factorX.moon=3;
+        factorX.planet=2;
     }
 }
